40ac8df9b9debef8c8c49e81e9d841c15cb39e4d
Satyam Jha <45529445+TH3-MA3STRO@users.noreply.github.com>
2019-05-25 00:15:11 +0530

    Automated scrolling of contacts

--- whatsappspammer.py
+++ whatsappspammer.py
@@ -1,32 +1,34 @@
+# imports
 from selenium import webdriver
-
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
+# user inputs
 count = int(input("Enter the number of msgs you wnat to spam: "))
 msg = input('Enter the msg you wnat to spam:')
 type_ch = input("Please specify the type of chat i.e. Group or a Contact: ").lower()
-
+# script
 driver = webdriver.Chrome()
 driver.get("https://web.whatsapp.com")
 
-time.sleep(20)  # Can be changed according to user's internet speed
-
+time.sleep(10)  # Can be changed according to user's internet speed
+driver.maximize_window()
 print('count complete')
 if type_ch == "contact":
     con_name = input("input contact name properly: ")
-    ''' note: can only select the contact which is shown on the first screen, if you want select any other contact then 
-    please scroll till you see that contact
-    '''
+
     time.sleep(2)
-    driver.find_element_by_xpath("//div[@class='_25Ooe']/span[@class='_3TEwt']/span[@title='{}']".format(con_name)).click()
+    ActionChains(driver).move_to_element(driver.find_element_by_xpath(
+        "//div[@class='_25Ooe']/span[@class='_3TEwt']/span[@title='{}']".format(con_name)).click()
+    )
+
 
 elif type_ch == "group":
     g_name = input("input Group's name properly: ")
-    ''' note: can only select the group which is shown on the first screen, if you want select any other group then 
-    please scroll till you see that contact
-    '''
+
     time.sleep(2)
-    driver.find_element_by_xpath("//div[@class='_25Ooe']/span[@title='{}']".format(g_name)).click()
+    ActionChains(driver).move_to_element(
+        driver.find_element_by_xpath("//div[@class='_25Ooe']/span[@title='{}']".format(g_name)).click())
 
 for i in range(0, count):
     driver.find_element_by_xpath("//div[@class='_2S1VP copyable-text selectable-text']").send_keys(msg)
@@ -34,3 +36,7 @@ for i in range(0, count):
 
     driver.find_element_by_xpath("//button[@class='_35EW6']/span").click()
     time.sleep(1)
+
+print("Script Complete! ")
+time.sleep(6)
+driver.__exit__()
