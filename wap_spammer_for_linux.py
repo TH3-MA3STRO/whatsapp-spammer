8ec3064df3aa975173acd2c8f906e09c1433c729
Satyam Jha <45529445+TH3-MA3STRO@users.noreply.github.com>
2019-07-15 00:14:48 +0530

    Added Auto-Login feature and several other fixes

--- wap_spammer_for_linux.py
+++ wap_spammer_for_linux.py
@@ -1,7 +1,12 @@
 # imports
 from selenium import webdriver
-
+from selenium.webdriver.common.action_chains import ActionChains
+import selenium
 import time
+
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
 import pyautogui
 
@@ -10,45 +15,60 @@ count = int(input("Enter the number of messages you want to spam: "))
 
 type_ch = input("Please specify the type of chat i.e. Group or a Contact: ").lower()
 
-# script
-driver = webdriver.Chrome()
+# driver configs
+options = webdriver.ChromeOptions()
+options.add_argument('--user-data-dir=./User_Data')
+driver = webdriver.Chrome(executable_path=r'''/home/chromedriver''', chrome_options=options)
+act = ActionChains(driver)
 driver.get("https://web.whatsapp.com")
 
+act = ActionChains(driver)
+wait = WebDriverWait(driver, 40)
 time.sleep(10)  # Can be changed according to user's internet speed
 
-if type_ch == "contact":
-    con_name = pyautogui.prompt("Enter Contact's Name")
-    driver.find_element_by_xpath(
-        "//div[@class='ZP8RM']/label[@class='eiCXe']/input[@class='_2zCfw copyable-text selectable-text']").click()
+# script
 
-    pyautogui.typewrite(con_name)
-    time.sleep(2)
+try:
+    if type_ch == "contact":
+        con_name = pyautogui.prompt("Enter Contact's Name")
+        wait.until(ec.visibility_of_element_located((By.XPATH, "//div[@class='ZP8RM']/label[@class='eiCXe']/input[@class='_2zCfw copyable-text selectable-text']")))
+        driver.find_element_by_xpath(
+            "//div[@class='ZP8RM']/label[@class='eiCXe']/input[@class='_2zCfw copyable-text selectable-text']").send_keys(
+            con_name)
 
-    driver.find_element_by_xpath("//span[@class='_3NWy8']/span[@class='_19RFN']/span[@class='matched-text']").click()
+        time.sleep(2)
+        wait.until(ec.visibility_of_element_located((By.XPATH, "//span[@class='_3NWy8']/span[@class='_19RFN']/span["
+                                                               "@class='matched-text']")))
 
-elif type_ch == "group":
-    g_name = pyautogui.prompt("Enter Group's Name")
-    driver.find_element_by_xpath("//div[@class='ZP8RM']/label[@class='eiCXe']/input[@class='_2zCfw copyable-text "
-                                 "selectable-text']").click()
-    pyautogui.typewrite(g_name)
+        driver.find_element_by_xpath(
+            "//span[@class='_3NWy8']/span[@class='_19RFN']/span[@class='matched-text']").click()
 
-    time.sleep(2)
-    driver.find_element_by_xpath("//div[@class='_2WP9Q']/div[@class='KgevS']/div[@class='_3H4MS']/span["
-                                 "@class='_19RFN']/span[@class='matched-text']").click()
+    elif type_ch == "group":
+        g_name = pyautogui.prompt("Enter Group's Name")
+        wait.until(ec.visibility_of_element_located((By.XPATH,
+                                                     "//div[@class='ZP8RM']/label[@class='eiCXe']/input[@class='_2zCfw copyable-text selectable-text']")))
+        driver.find_element_by_xpath("//div[@class='ZP8RM']/label[@class='eiCXe']/input[@class='_2zCfw copyable-text "
+                                     "selectable-text']").send_keys(g_name)
+        time.sleep(2)
+        wait.until(ec.visibility_of_element_located((By.XPATH,
+                                                     "//div[@class='_2WP9Q']/div[@class='KgevS']/div[@class='_3H4MS']/span[@class='_19RFN']/span[@class='matched-text']")))
+        driver.find_element_by_xpath("//div[@class='_2WP9Q']/div[@class='KgevS']/div[@class='_3H4MS']/span["
+                                     "@class='_19RFN']/span[@class='matched-text']").click()
 
-msg = pyautogui.prompt("Enter the msg you wish to spam")
-for i in range(0, count):
-    driver.find_element_by_xpath(
-        "//div[@id='main']/footer[@class='_1N6pS']/div[@class='_2i7Ej copyable-area']/div[@class='_13mgZ']").click()
+    msg = pyautogui.prompt("Enter the msg you wish to spam")
+    for i in range(0, count):
+        driver.find_element_by_xpath(
+            "//div[@id='main']/footer[@class='_1N6pS']/div[@class='_2i7Ej copyable-area']/div[@class='_13mgZ']").send_keys(
+            msg)
+        time.sleep(0.25)
 
-    time.sleep(1)
+        driver.find_element_by_xpath("//div[@class='hnQHL'][2]/button[@class='_3M-N-']/span").click()
+        time.sleep(0.5)
 
-    pyautogui.typewrite(msg)
+    pyautogui.alert('The chat was bombed successfully!')
 
-    driver.find_element_by_xpath("//div[@class='hnQHL'][2]/button[@class='_3M-N-']/span").click()
-
-    time.sleep(1)
-
-pyautogui.alert('The chat was bombed successfully!')
+except selenium.common.exceptions.WebDriverException:
+    pyautogui.alert("Some error occurred :(")
+    time.sleep(10)
 
 driver.__exit__()
