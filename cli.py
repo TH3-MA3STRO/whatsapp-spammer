a4257be7a740ef8dabba79a1d090f43d8e8149e1
Satyam Jha <45529445+TH3-MA3STRO@users.noreply.github.com>
2019-07-21 15:15:43 +0530

    Add files via upload

---
+++ cli.py
@@ -0,0 +1,74 @@
+from pyfiglet import Figlet
+f = Figlet(font='slant')
+
+from PyInquirer import style_from_dict, Token, prompt, Separator
+from PyInquirer import Validator, ValidationError
+print(f.renderText('Whatsapp -\nspammer'))
+print('''
+********************************************************
+*                                                      *
+*    Author: TH3-MA3STRO                               *
+*    Instagtam: @th3_MA3STRO                           *
+*    GitHub: https://www.github.com/TH3-MA3STRO        *
+*    Platform: Windows                                 *
+*                                                      *
+********************************************************
+''')
+
+class NumberValidator(Validator):
+    def validate(self, document):
+        try:
+            int(document.text)
+        except ValueError:
+            raise ValidationError(
+                message='Please enter a number',
+                cursor_position=len(document.text))  # Move cursor to end
+
+
+def styler():
+    style = style_from_dict({
+        Token.Separator: '#cc5454',
+        Token.QuestionMark: '#673ab7 bold',
+        Token.Selected: '#cc5454',  # default
+        Token.Pointer: '#673ab7 bold',
+        Token.Instruction: '',  # default
+        Token.Answer: '#f44336 bold',
+        Token.Question: '',
+    })
+    return style
+
+
+def que1():
+
+    questions = [
+        {
+            'type': 'list',
+            'message': 'Select the type of chat:',
+            'name': 'tch',
+            'choices': [
+                {
+                    'name': 'Group'
+                },
+                {
+                    'name': 'Contact'
+                }
+            ]
+        }
+    ]
+    answers = prompt(questions, style=styler())
+    return answers
+
+
+def que2():
+    questions2 = [
+        {
+            'type': 'input',
+            'message': 'Enter the Numer of messages you wish to spam:',
+            'name': 'num',
+            'validate': NumberValidator
+        }
+
+    ]
+    numo = prompt(questions2, style=styler())
+    return numo
+
